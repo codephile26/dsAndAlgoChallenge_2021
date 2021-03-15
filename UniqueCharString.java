cf66273e7ced2ca87716aa882c4c469713d16415
shahd <dimple.shah@tomtom.com>
2021-03-15 21:14:36 +0530

    Adding a better method to check if a string has all unique characters - based on maintaining an array of boolean values, based on assumption that the input string is an ascii string

--- UniqueCharString.java
+++ UniqueCharString.java
@@ -7,8 +7,10 @@ public class UniqueCharString{
 		Scanner sc = new Scanner(System.in);
 		String inputString = sc.nextLine();
 		System.out.println(checkIfAllCharactersUnique(inputString));
+		System.out.println(checkIfAllCharactersUnique_hashArray(inputString));
 	}
 	
+	// has a complexity of O(nlogn), not a very good approach if the sorting algorithm takes too long over here, or for large strings
 	public static boolean checkIfAllCharactersUnique(String inputString){
 		char [] inputStringArray = inputString.toCharArray();
 		Arrays.sort(inputStringArray);
@@ -20,5 +22,21 @@ public class UniqueCharString{
 			previousChar = inputStringArray[i];
 		}
 		return true;		
+	}
+
+	//based on assumption that the input string is an ASCII string, time complexity = O(n), space complexity = 1
+	public static boolean checkIfAllCharactersUnique_hashArray(String inputString){
+		if (inputString.length() > 128) {
+			return false;
+		}
+		boolean [] hashArray = new boolean[128];
+		for (int i = 0; i<inputString.length(); i++){
+			int val = inputString.charAt(i);
+			if (hashArray[val]){
+				return false;
+			}
+			hashArray[val] = true;
+		}
+		return true;
 	}
 }
