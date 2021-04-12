53863e5aa9aa33d3abab23c6db4274e4e1f9ab97
shahd <dimple.shah@tomtom.com>
2021-04-12 14:24:17 +0530

    Adding space efficient way to add zeros in a matrix, if there are elements with value equal to zero in it

--- ZeroMatrix.java
+++ ZeroMatrix.java
@@ -13,7 +13,7 @@ public class ZeroMatrix {
 			}
 		}
 		
-		setZeros(matrix);
+		setZerosSpaceEfficient(matrix);
 
 		for (int i = 0; i < m; i++){
 			for (int j = 0; j < n; j++){
@@ -59,6 +59,57 @@ public class ZeroMatrix {
 		for (int i = 0; i < matrix.length; i++){
 			matrix[i][column] = 0;
 		}
+	}
+
+	public static void setZerosSpaceEfficient(int [][] matrix){
+		boolean rowHasZero = false;
+		boolean colHasZero = false;
+
+		//check for zeros in first row
+		for (int j = 0; j < matrix[0].length; j++) {
+			if (matrix[0][j] == 0){
+				rowHasZero = true;
+				break;
+			}
+		}
+
+		//check for zeros in first column
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[i][0] == 0){
+				colHasZero = true;
+				break;
+			}
+		}
+
+		// iterate through the rest of the matrix to check for other zeros
+		// when a zero is found, make the first element of that row and the first element of that column equal to zero
+		for (int i = 1; i < matrix.length; i++){
+			for (int j = 1; j < matrix[0].length; j++){
+				if (matrix[i][j]== 0) {
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+				}
+			}
+		}
+
+		//nullify row, based on value in first column
+		for (int i = 1; i < matrix.length; i++){
+			if (matrix[i][0] == 0){
+				nullifyRow(i, matrix[i]);
+			}
+		}
+
+		//nullify column, based on value in first row
+		for (int j = 1; j < matrix[0].length; j++){
+			if (matrix[0][j] == 0){
+				nullifyColumn(j,matrix);
+			}
+		}
+
+		// nullify first row and column, if needed
+		if (rowHasZero) nullifyRow(0,matrix[0]);
+		if (colHasZero) nullifyColumn(0, matrix);
+
 	}
 
 }
