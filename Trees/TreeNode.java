2f3b0a1bfe95744a4901fd78178dadb897065222
codephile26 <dimples2609@gmail.com>
2021-05-01 15:35:29 +0530

    Renaming Node class to TreeNode to represent a generic node of a tree

---
+++ Trees/TreeNode.java
@@ -0,0 +1,28 @@
+public class TreeNode<T>{
+	private T data;
+	private TreeNode left, right;
+
+	public void setData(T data){
+		this.data = data;
+	}
+
+	public T getData(){
+		return this.data;
+	}
+
+	public TreeNode getLeft(){
+		return left;
+	}
+
+	public void setLeft(TreeNode<T> left){
+		this.left = left;
+	}
+
+	public void setRight(TreeNode<T> right){
+		this.right = right;
+	}
+
+	public TreeNode getRight(){
+		return right;
+	}
+}
