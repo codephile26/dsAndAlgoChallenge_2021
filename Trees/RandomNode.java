d5a85b51434696baec0c311fab99415527035d8d
codephile26 <dimples2609@gmail.com>
2021-05-12 20:55:31 +0530

    Optimizing random node method to reduce random number generation. Created a wrapper tree class to reduce the random number calls.

--- Trees/RandomNode.java
+++ Trees/RandomNode.java
@@ -47,17 +47,14 @@ class TreeNode {
 		size++;
 	}
 
-	public TreeNode getRandomNode(){
+	public TreeNode getIthNode(int index){
 		int leftSize = (left != null) ? left.size() : 0;
-		Random randomIndex = new Random();
-		int index = randomIndex.nextInt(size);
-		System.out.println("random index " + index);
 		if (index < leftSize){
-			return left.getRandomNode();
+			return left.getIthNode(index);
 		} else if (index == leftSize){
 			return this;
 		} else {
-			return right.getRandomNode();
+			return right.getIthNode(index - (leftSize + 1));
 		}
 	}
 
@@ -67,17 +64,43 @@ class TreeNode {
 	public TreeNode getRight(){ return this.right;}
 }
 
+class Tree {
+	//size, getRandomNode - optimized to reduce random number calls, insertInOrder
+
+	TreeNode root = null;
+
+	public int size(){
+		return (root == null) ? 0 : root.size();
+	}
+
+	public TreeNode getRandomNode(){
+		if (root == null){
+			return null;
+		}
+		Random random = new Random();
+		int index = random.nextInt(size());
+		return root.getIthNode(index);
+	}
+
+	public void insertInOrder(int d){
+		if (root == null){
+			root = new TreeNode(d);
+		} else {
+			root.insertInOrder(d);
+		}
+	}
+}
+
 public class RandomNode {
 	public static void main(String...ar){
+		Tree tree = new Tree();
 		TreeNode treeNode = new TreeNode(5);
-		treeNode.insertInOrder(1);
-		treeNode.insertInOrder(3);
-		treeNode.insertInOrder(6);
-		treeNode.insertInOrder(7);
-		TreeNode randomNode = treeNode.getRandomNode();
-		System.out.println(treeNode.size());
-		System.out.println(treeNode.getLeft().size());
-		System.out.println(treeNode.getRight().size());
+		tree.root = treeNode;
+		tree.insertInOrder(1);
+		tree.insertInOrder(3);
+		tree.insertInOrder(6);
+		tree.insertInOrder(7);
+		TreeNode randomNode = tree.getRandomNode();
 		System.out.println("Random Node data:" + randomNode.data());
 	}
 }
