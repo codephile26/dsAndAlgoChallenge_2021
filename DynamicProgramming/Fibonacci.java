6ab795ca0f90307f6105c7dae5158e7ef0894461
codephile26 <dimples2609@gmail.com>
2021-05-20 20:01:22 +0530

    Fibonacci - bottom up - both the approaches are a great improvement in runtime over the traditional recursive method used

--- DynamicProgramming/Fibonacci.java
+++ DynamicProgramming/Fibonacci.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Fibonacci{
 
+	/* Time complexity is O(n)
+	 *
+	 * Space Complexity is O(n)
+	 *
+	 */
 	public static int fibonacciMemoization(int n){
 		return fibonacciMemoization(n, new int[n + 1]);
 	}
@@ -19,6 +24,24 @@ public class Fibonacci{
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println(n +"th number in Fibonacci Sequence is " + fibonacciMemoization(n));
+		System.out.println(n +"th number in Fibonacci Sequence is " + fibonacciBottomUp(n));
 	}
 
+	/* Time complexity is O(n)
+	 *
+	 * Space Complexity is O(1)
+	 *
+	 */
+	public static int fibonacciBottomUp(int n){
+		if (n == 0) return 1;
+		if (n == 1) return 1;
+		int a = 0;
+		int b = 1;
+		for (int i = 2; i <=n; i++){
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+		return a + b;
+	}
 }
