c47b9ff3bd1bfe5a83ac566abf941131d1a3dedd
codephile26 <dimples2609@gmail.com>
2021-06-15 21:01:44 +0530

    Sliding window question to find the first negative number in a window of size k

---
+++ SlidingWindow/FirstNegativeElementInWindow.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+
+public class FirstNegativeElementInWindow{
+
+	public static int[] findFirstNegativeElementInWindow(int [] arr, int k){
+		int i;
+		int [] result = new int[arr.length - k + 1];
+		LinkedList<Integer> list = new LinkedList<>();
+		//Before hitting the size of the sliding window
+		for (i = 0; i < k;i++){
+			if (arr[i] < 0){
+				list.add(i);
+			}
+		}
+
+		// calculations after hitting the sliding window
+		for (; i < arr.length; i++){
+			if (!(list.isEmpty())){
+				result[i - k] = list.peek();
+			} else {
+				result[i - k] = 0;
+			}
+
+			while((!list.isEmpty()) && list.peek() < (i - k + 1)){
+				list.remove();
+			}
+
+			if (arr[i] < 0){
+				list.add(i);
+			}
+		}
+
+		if (!list.isEmpty()){
+			result[i] = list.peek();
+		} else {
+			result[i] = 0;
+		}
+
+		return result;
+	}
+
+	public static void main(String...ar){
+		int [] arr = new int[]{12,-1,-7,8,-15,30,16,28};
+		System.out.print(findFirstNegativeElementInWindow(arr,3));
+	}
+
+
+
+}
